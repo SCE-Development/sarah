b1f9711e2bbd289d538d29eb6ad2b973d6d709fe
Aaron Truong <aaron@Aarons-MacBook-Pro.local>
2025-01-14 20:28:05 -0800

    readded config.example.json and fixed linting

--- src/commands/util/devteam.js
+++ src/commands/util/devteam.js
@@ -2,14 +2,16 @@ const Command = require('../Command');
 
 
 module.exports = new Command({
-  name: 'devteam',
-  description: 'Devteam information doc',
-  aliases: [],
-  example: 's!devteam',
-  permissions: 'general',
-  category: 'information',
-  // eslint-disable-next-line
-  execute: async (message, args) => {
-      const devteam = await message.channel.send('https://sce.sjsu.edu/s/joinus');
-  },
+    name: 'devteam',
+    description: 'Devteam information doc',
+    aliases: [],
+    example: 's!devteam',
+    permissions: 'general',
+    category: 'information',
+    // eslint-disable-next-line
+    execute: async (message, args) => {
+        await message.channel.send(
+            'https://sce.sjsu.edu/s/joinus'
+        );
+    },
 });
--- src/commands/util/internship.js
+++ src/commands/util/internship.js
@@ -2,14 +2,16 @@ const Command = require('../Command');
 
 
 module.exports = new Command({
-  name: 'internship',
-  description: 'Internship information doc',
-  aliases: [],
-  example: 's!internship',
-  permissions: 'general',
-  category: 'information',
-  // eslint-disable-next-line
-  execute: async (message, args) => {
-      const internship = await message.channel.send('https://docs.google.com/document/d/1hqSKPdD8BYJs1Oqc1AWJoJg5hovUv-IR_NVx9QSqPSo/edit?usp=sharing');
-  },
+    name: 'internship',
+    description: 'Internship information doc',
+    aliases: [],
+    example: 's!internship',
+    permissions: 'general',
+    category: 'information',
+    // eslint-disable-next-line
+    execute: async (message, args) => {
+        await message.channel.send(
+            'https://docs.google.com/document/d/1hqSKPdD8BYJs1Oqc1AWJoJg5hovUv-IR_NVx9QSqPSo/edit?usp=sharing'
+        );
+    },
 });
