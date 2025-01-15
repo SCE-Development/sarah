7d958ed1b452b90530ebe59555242409cb2dd4d6
Aaron Truong <aaron@Aarons-MacBook-Pro.local>
2025-01-14 20:31:14 -0800

    fixed spacing

--- src/commands/util/internship.js
+++ src/commands/util/internship.js
@@ -2,16 +2,16 @@ const Command = require('../Command');
 
 
 module.exports = new Command({
-    name: 'internship',
-    description: 'Internship information doc',
-    aliases: [],
-    example: 's!internship',
-    permissions: 'general',
-    category: 'information',
-    // eslint-disable-next-line
-    execute: async (message, args) => {
-        await message.channel.send(
-            'https://docs.google.com/document/d/1hqSKPdD8BYJs1Oqc1AWJoJg5hovUv-IR_NVx9QSqPSo/edit?usp=sharing'
-        );
-    },
+  name: 'internship',
+  description: 'Internship information doc',
+  aliases: [],
+  example: 's!internship',
+  permissions: 'general',
+  category: 'information',
+  // eslint-disable-next-line
+  execute: async (message, args) => {
+    await message.channel.send(
+      'https://sce.sjsu.edu/s/internshipinfo'
+    );
+  },
 });
