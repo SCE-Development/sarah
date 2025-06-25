9f1b930cf39cfc9a8bd57a89b749116154edbb7e
Rachel Tran <tranracheln@gmail.com>
2025-06-24 21:56:02 -0700

    Use buttons to update role

--- src/handlers/ReactionHandler.js
+++ src/handlers/ReactionHandler.js
@@ -1,6 +1,7 @@
 const { REACTIONS = {} } = require('../../config.json');
-const { EmbedBuilder } = require('discord.js');
 const logger = require('../util/logger');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+
 /**
 * Add a reaction handler to the handler list.
 * @param {Message} message - The message to add reaction to.
@@ -8,54 +9,69 @@ const logger = require('../util/logger');
 * @param {boolean} reactionWasRemoved - Whether reaction was removed
 * @param {string} botpfp - Bot's profile picture
 */
-class ReactionHandler {
-  async handleReaction(reaction, user, botpfp, reactionWasRemoved = false) {
-    const emoji = reaction.emoji.name;
-    const member = reaction.message.guild.members.cache.get(user.id);
 
+
+
+class ReactionHandler {
+
+  //MessageReaction object, User object, botpfp object is passed in
+  //reaction object has emoji, message, users properties
+
+  async handleReaction(reaction, user, botpfp, reactionWasRemoved = false) {
+    //this makes sure that we are only executing the below code when a reaction happens
+    //on the specific message that we want stuff to happen
+    //we define the REACTIONS object in config.json, where specific messages of our choosing
+    //contain maps from emojis to role ids
+    //this is basically checking if REACTIONS contains the message id of the reaction that just happened
     if (!REACTIONS[reaction.message.id]) {
       return;
     }
+    //what dis function needs to do is
+    //find the user
+    //make an embed with a message such as "Welcome to SCE discord server react with thumps up if u a real one"
+    //dm the user that embed
+
+    //this returns a GuildMember object
+    //we need a GuildMember object to assign roles to the member, but like
+    //to dm them we lowkey don't need dat, we just need the user.id lolz
+    const member = reaction.message.guild.members.cache.get(user.id);
+
+    const guildName = reaction.message.guild.name;
+    const emoji = reaction.emoji.name;
+
 
     try {
-      const role = reaction.message.guild.roles.cache.get(
-        REACTIONS[reaction.message.id][emoji]
-      );
-      // If reverse is true, flip. If not keep as is
-      let roleAction = reactionWasRemoved;
-      if (REACTIONS[reaction.message.id].reverse) {
-        roleAction = !roleAction;
-      }
+      //roles.cache.get(roleId) will give you the whole role object from the role id
+      const role = reaction.message.guild.roles.cache.get(REACTIONS[reaction.message.id][emoji]);
 
-      const guildName = reaction.message.guild.name;
+      //makes an embed with our message
       const embed = new EmbedBuilder()
-        .setTitle('Roles Updated')
+        .setTitle('Hey Queen! Welcome to the SCE Discord Server!')
+        .setDescription('psssst.... hey kid......' + 'click on the button for a role......')
         .setFooter({
-          text: 'Sent by the Reaction Roles bot'
-            + ` on behalf of the server, ${guildName}`,
+          text: 'Sent by the Reaction Roles bot' +
+          `on behalf of the server, ${guildName}`,
           iconURL: `${botpfp}`
         });
-        
-      let verb = 'reacted';
-      let addedOrRemoved = 'Added';
-      if (roleAction) {
-        member.roles.remove(role);
-        verb = 'removed your reaction';
-        addedOrRemoved = 'Removed';
-      } else {
-        member.roles.add(role);
-      }
-      const name = `Roles ${addedOrRemoved}`;
-      let description = `You ${verb} to this ` + 
-        `[this message](${reaction.message.url}) in the server, `
-        + `${guildName}, and changed your roles.`;
-      embed.setDescription(description);
-      embed.addFields(
-        { name, value: `${role.name}` }
-      );
-      await member.send({embeds: [embed]});
+      
+      //makes a button
+      const button = new ButtonBuilder()
+      //whenever the button is pressed, an interaction event is sent to the bot
+      //we set the id of the interaction event to be the roleid
+      //this allows us to handle this specific interaction in our server.js
+        .setCustomId(`role_toggle_${role.id}`)  // unique ID per role
+        .setLabel(role.name)
+        .setStyle(ButtonStyle.Primary);
+      
+      //adds the button to an action row
+      const row = new ActionRowBuilder()
+        .addComponents(button);
+
+      //sends a dm to the member with the embed
+      //the embed will also have the action 
+      await member.send({embeds: [embed], components: [row]})
     } catch (e) {
-      logger.warn('Role does not exist', e);
+      logger.warn('ik im not your type but can i be the EXCEPTION hehehehe: ', e)
     }
   }
 }
