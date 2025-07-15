7617c4a8aa11f2ac6025f125e8e7cc474ccd2a89
ra2y <tranracheln@gmail.com>
2025-07-14 20:26:26 -0700

    Use buttons to update role (#218)
    * Use buttons to update role
    
    * add verify command
    
    * added verify dm to new member add handler
    
    * fix lint errors
    
    * fixed more lint errors
    
    * more lint error fixing
    
    * lint error
    
    * fix verify command
    
    * fix lint error
    
    * revert reaction handler to original
    
    * fix the lint error
    
    * fix config.json filepath
    
    * update config example
    
    * Apply suggestions from code review
    
    ---------
    
    Co-authored-by: Evan Ugarte <36345325+evanugarte@users.noreply.github.com>

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const logger = require('./src/util/logger');
 const {
   prefix,
   API_TOKEN,
+  guildId,
 } = require('./config.json');
+
 const { MessageHandler } = require('./src/handlers/MessageHandler');
 const {
   VoiceChannelChangeHandler,
@@ -13,6 +15,7 @@ const {
 const { NewMemberAddHandler } = require('./src/handlers/NewMemberAddHandler');
 const { MemberLeaveHandler } = require('./src/handlers/MemberLeaveHandler');
 const { ReactionHandler } = require ('./src/handlers/ReactionHandler');
+
 
 const PROMETHEUS_PORT = process.env.PROMETHEUS_PORT || 9000;
 
@@ -54,6 +57,9 @@ const startBot = async () => {
   const vcChangeHandler = new VoiceChannelChangeHandler();
   const newMemberHandler = new NewMemberAddHandler();
   const memberLeaveHandler = new MemberLeaveHandler();
+  // creating a new ReactionHandler object
+  // we can call the methods defined in the ReactionHandler class by 
+  // calling them on the reactionHandler object
   const reactionHandler = new ReactionHandler();
   client.once('ready', () => {
     messageHandler.initialize();
@@ -74,6 +80,7 @@ const startBot = async () => {
     vcChangeHandler.handleChangeMemberInVoiceChannel(oldState, newState);
   });
 
+  // newMember is a GuildMember object
   client.on('guildMemberAdd', (newMember) => {
     newMemberHandler.handleNewMember(newMember);
   });
@@ -81,6 +88,26 @@ const startBot = async () => {
   client.on('guildMemberRemove', (member) => {
     memberLeaveHandler.handleMemberLeave(member);
   });
+  // client.on basically triggers a function whenever an event happens
+  // checks to see if a reaction has been added to a message in the server
+  // whenever a messageReactionAdd event happens
+  // Discord.js sends MessageReaction object, and User object
+  // into our specified function
+  // passes in the MessageReaction object and User object containing data
+  // the reaction and the user are passed into the handleReaction method
+  // from the reactionHandler class
+
+  // we can use reaction.message, to get the message object, or reaction.emoji
+  // we can use message.id, or message.content, to access information about 
+  // the message
+  // user is a User object, and contains information about the person 
+  // that reacted
+  // we can use user.id or user.username on the User object
+  // everything is a json........... MessageReaction...... User......... 
+  // Message........
+  // what if WE yes me and you are jsons too......... 
+  // look at the love inside my heart for you with 
+  // JSON.stringify(me.heart) vro.......
 
   client.on('messageReactionAdd', async (reaction, user) => {
     const botpfp = client.user.displayAvatarURL();
@@ -92,6 +119,51 @@ const startBot = async () => {
     reactionHandler.handleReaction(reaction, user, botpfp, true);
   });
 
+  client.on('interactionCreate', async interaction => {
+    if (!interaction.isButton()) return; // Only handle button interactions
+
+    const customId = interaction.customId;
+
+    // Check if this is one of our role toggle buttons
+    if (!customId.startsWith('role_toggle_'))
+      return;
+
+    const roleId = customId.replace('role_toggle_', '');
+
+    // Replace with your actual guild ID where roles exist
+    const guild = client.guilds.cache.get(guildId);
+
+    if (!guild) {
+      return interaction.reply({ content: 'Guild not found.', 
+        ephemeral: true });
+    }
+
+    try {
+      // Fetch member from the guild
+      const member = await guild.members.fetch(interaction.user.id);
+
+      // Get the role from the guild's cache
+      const role = guild.roles.cache.get(roleId);
+
+      if (!role) {
+        return interaction.reply({ content: 'Role not found.', 
+          ephemeral: true });
+      }
+
+      if (member.roles.cache.has(roleId)) {
+        await member.roles.remove(role);
+        await interaction.reply({ content: 'Removed the role ' + 
+          `**${role.name}** from you.`, ephemeral: true });
+      } else {
+        await member.roles.add(role);
+        await interaction.reply({ content: 'Added the role ' + 
+          `**${role.name}** to you.`, ephemeral: true });
+      }
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'There was an error while' +
+        'updating your roles.', ephemeral: true });
+    }});
   client.login(API_TOKEN);
 };
 
---
+++ src/commands/util/verify.js
@@ -0,0 +1,74 @@
+const Command = require('../Command');
+const config = require('../../../config.json');
+const { EmbedBuilder, ActionRowBuilder, 
+  ButtonBuilder, ButtonStyle } = require('discord.js');
+
+
+module.exports = new Command({
+  name: 'verify',
+  description: 'sends a message to the user to verify them',
+  aliases: [],
+  example: 'r!verify',
+  permissions: 'general',
+  category: 'information',
+  execute: async(message) => {
+    // this will return a GuildMember object
+    const member = message.member;
+
+    // this gets the name of the server
+    const guildName = message.guild.name;
+
+    let memberName = message.member.nickname;
+    if (message.member.nickname === null) {
+      memberName = message.member.user.username;
+    }
+    // const botpfp = client.user.displayAvatarURL();
+    try {
+      // gets the role object by its id
+      const role = message.guild.roles.cache.get(config.verifiedRoleId);
+      const embed = new EmbedBuilder()
+        .setTitle('Who is this diva!?? Welcome to the' + `
+          SCE Discord Server ${memberName}!`)
+        .setDescription('psssst.... hey queen......' + 
+          'click on the button to verify yourself ' + 
+          'and get access to the rest of the server......')
+        .setFooter({
+          text: 'Sent by the Reaction Roles bot' +
+          `on behalf of the server, ${guildName}`
+        });
+
+      const button = new ButtonBuilder()
+        .setCustomId(`role_toggle_${role.id}`)
+        .setLabel(role.name)
+        .setStyle(ButtonStyle.Primary);
+
+      const row = new ActionRowBuilder()
+        .addComponents(button);
+        
+      // attempts to send the embed as a dm first
+      await member.send({embeds: [embed], components: [row]});
+
+      // if there is an error when sending, 
+      // won't reply with the confirmation msg in the server
+      return await message.reply('mamacita check your dm');
+    } catch (e) {
+      console.log('ik im not your type but can i ' +
+        'be the EXCEPTION hehehehe: ', e);
+      // if the error object caught has a code of 50007
+      // that means that it is the 'cannot send message to the user error'
+      if (e.code === 50007) {
+        const replyEmbed = new EmbedBuilder()
+        // send an appropriate error message 
+        // replying to the user in the server
+          .setDescription(`uhhhhhh ${memberName} mamacita, ` + 
+            'i can\'t dm you,,,,,, can you check your settings')
+        // with an appropriate gif
+          .setImage('https://media0.giphy.com/media/v1.Y2lkPTc5' +
+            'MGI3NjExcDkzbWdqNDU5NDl2ZGNsZDRxOWc4bDlsejIwZHJvaXB' +
+            'wdzJua3hydCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw' +
+            '/YAnpMSHcurJVS/giphy.gif');
+        return await message.reply({embeds: [replyEmbed]});
+      }
+    }
+  }
+});
