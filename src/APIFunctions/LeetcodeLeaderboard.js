e9062e8e5bbd6b40b438328f9c221c741a7f2d06
adarshm11 <amlearning2023@gmail.com>
2025-12-29 13:14:35 -0800

    lc leaderboard embed

--- src/APIFunctions/LeetcodeLeaderboard.js
+++ src/APIFunctions/LeetcodeLeaderboard.js
@@ -11,20 +11,10 @@ function queryLeetcodeLeaderboard() {
       LEETCODE_LEADERBOARD_API_URL,
     )
       .then((res) => {
-        const responseData = res.data.map((user, index) => {
-          const profileUrl = `https://leetcode.com/u/${user.user}/`;
-          return (
-            `${index + 1}. [${user.user}](${profileUrl}): ` +
-              (user.points > 1
-                ? user.points + ' points'
-                : user.points + ' point')
-          );
-        });
-        const trimmedResponseData = responseData.slice(0, 10).join('\n');
-        resolve(trimmedResponseData);
+        resolve(res.data);
       })
       .catch(() => {
-        resolve('Error querying LeetCode Leaderboard.');
+        resolve(null);
       });
   });
 }
